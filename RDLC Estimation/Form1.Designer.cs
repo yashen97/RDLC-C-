d56cdca40f5b6691b70b72fb45a2203d68a2a6df
Yashen Raveesha Samarasingha <45714091+yashen97@users.noreply.github.com>
2020-07-17 18:59:58 +0530

    Add files via upload

---
+++ RDLC Estimation/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace RDLC_Estimation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Print_EST = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "RDLC_Estimation.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(12, 81);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(776, 513);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(266, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Print_EST
+            // 
+            this.Print_EST.Location = new System.Drawing.Point(603, 24);
+            this.Print_EST.Name = "Print_EST";
+            this.Print_EST.Size = new System.Drawing.Size(75, 23);
+            this.Print_EST.TabIndex = 2;
+            this.Print_EST.Text = "Print";
+            this.Print_EST.UseVisualStyleBackColor = true;
+            this.Print_EST.Click += new System.EventHandler(this.Print_EST_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 606);
+            this.Controls.Add(this.Print_EST);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Print_EST;
+    }
+}
+
---
+++ RDLC Estimation/Form1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDLC_Estimation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void Print_EST_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=YashenGarage;Integrated Security=True";
+
+
+            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlDataAdapter da = new SqlDataAdapter("select * from Estimation_List where Master_ID = '" + textBox1.Text + "'", con);
+            DataSet1 ds = new DataSet1();
+            da.Fill(ds, "DataTable1");
+
+            ReportDataSource datasource = new ReportDataSource("DataSet1", ds.Tables[0]);
+
+            this.reportViewer1.LocalReport.DataSources.Clear();
+            this.reportViewer1.LocalReport.DataSources.Add(datasource);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
